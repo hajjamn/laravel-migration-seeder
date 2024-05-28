9ce5614d08c9f55637015f58296fc68820d66546
HajjamN <hajjamn@gmail.com>
2024-05-28 10:02:42 +0200

    FIX: fixed some properties of the seeder

--- database/seeders/TrainSeeder.php
+++ database/seeders/TrainSeeder.php
@@ -14,16 +14,18 @@ class TrainSeeder extends Seeder
      */
     public function run(Faker $faker): void
     {
+
+        $companies = ['Trenitalia', 'Trenord', 'Italo'];
         for( $i = 0; $i < 100; $i++) {
             //creiamo l'istanza del model Train
             $new_train = new Train();
 
             //popoliamo le proprieta` dell'istanza
-            $new_train->company = $faker->company();
+            $new_train->company = $faker->randomElement($companies);
             $new_train->departure_station = $faker->city();
             $new_train->arrival_station = $faker->city();
-            $new_train->departure_time = $faker->dateTimeBetween($startDate = '-1 day', $endDate = 'now');
-            $new_train->arrival_time = $faker->dateTimeBetween($startDate = 'now', $endDate = '+1 day');
+            $new_train->departure_time = $faker->dateTimeBetween('-1 day', '+1 day');
+            $new_train->arrival_time = $faker->dateTimeBetween('+1 day', '+2 day');
             $new_train->train_code = $faker->unique()->bothify('??#####');
             $new_train->wagons_number = $faker->numberBetween(1,10);
             $new_train->on_time = $faker->boolean();
